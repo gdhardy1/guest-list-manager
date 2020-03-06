6d524942c831296ae2f778ecdad870d48794de4a
Gregory D Hardy <gdhardy1@gmail.com>
2020-03-05 21:20:37 -0800

    rename application class

--- server/src/main/java/com/gdhardy/simplersvp/SimpleRsvpApp.java
+++ server/src/main/java/com/gdhardy/simplersvp/SimpleRsvpApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SimpleRsvpApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+    SpringApplication.run(SimpleRsvpApp.class, args);
 	}
 
 }
