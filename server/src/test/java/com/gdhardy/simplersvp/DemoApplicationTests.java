f8799c2189dadf3fe4dc23757cd02794f179f481
Gregory D Hardy <gdhardy1@gmail.com>
2020-03-05 20:20:00 -0800

    build maven project

---
+++ server/src/test/java/com/gdhardy/simplersvp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdhardy.simplersvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
