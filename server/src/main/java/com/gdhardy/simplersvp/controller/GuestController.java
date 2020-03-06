dc7a15ec60d63d0eb0956a043b960e5d10420968
Gregory D Hardy <gdhardy1@gmail.com>
2020-03-06 07:34:48 -0800

    inject GuestService and remove biz logic

--- server/src/main/java/com/gdhardy/simplersvp/controller/GuestController.java
+++ server/src/main/java/com/gdhardy/simplersvp/controller/GuestController.java
@@ -1,9 +1,9 @@
 package com.gdhardy.simplersvp.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.gdhardy.simplersvp.model.Guest;
+import com.gdhardy.simplersvp.service.GuestService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GuestController {
 
+  private final GuestService guestService;
+
+  GuestController(GuestService guestService){
+    this.guestService = guestService;
+  }
+
   @GetMapping(value="api/guests", produces="application/json")
   public List<Guest> findGuests(){
-    ArrayList<Guest> guests = new ArrayList<Guest>();
-    Guest guest = new Guest();
-    guest.setFirstName("James");
-    guests.add(guest);
-    return guests;
+    return guestService.findAllGuests();
   }
 }
