86f3cb09fdd51aa8e0cdc0a3e9033852fa014387
Gregory D Hardy <gdhardy1@gmail.com>
2020-03-07 13:03:42 -0800

    annotate for rsvp value serial-/deserialization

--- server/src/main/java/com/gdhardy/simplersvp/model/Rsvp.java
+++ server/src/main/java/com/gdhardy/simplersvp/model/Rsvp.java
@@ -1,8 +1,22 @@
 package com.gdhardy.simplersvp.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Rsvp
  */
 public enum Rsvp {
-  GOING, NOT_GOING, NO_RESPONSE
+  GOING("Going"), NOT_GOING("Not Going"), NO_REPLY("No Reply");
+
+  private String value;
+
+  Rsvp(String value){
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getReply(){
+    return value;
+  }
+
 };
