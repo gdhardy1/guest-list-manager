38a47fe3789e916ad56ce18ae0887a51517df73e
Gregory D Hardy <gdhardy1@gmail.com>
2020-03-11 11:49:54 -0700

    simplify app annotation

--- server/src/main/java/com/gdhardy/simplersvp/SimpleRsvpApp.java
+++ server/src/main/java/com/gdhardy/simplersvp/SimpleRsvpApp.java
@@ -1,13 +1,10 @@
 package com.gdhardy.simplersvp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication
 @ComponentScan("com.gdhardy.simplersvp")
 public class SimpleRsvpApp {
 
