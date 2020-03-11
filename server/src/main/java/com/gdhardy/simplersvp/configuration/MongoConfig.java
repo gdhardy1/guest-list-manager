a35e17cf780e20432f06a7f2f5f048b5ff28f64a
Gregory D Hardy <gdhardy1@gmail.com>
2020-03-11 11:49:29 -0700

    update configuration on per profile basis

--- server/src/main/java/com/gdhardy/simplersvp/configuration/MongoConfig.java
+++ server/src/main/java/com/gdhardy/simplersvp/configuration/MongoConfig.java
@@ -5,9 +5,11 @@ import com.mongodb.ConnectionString;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.mongodb.MongoClientSettings;
@@ -15,21 +17,23 @@ import com.mongodb.MongoClientSettings;
 /**
  * MongoConfig
  */
+@Profile({"prod"})
 @Configuration
 public class MongoConfig{
 
-  @Value("${mongodb.uri}")
+  @Value("${spring.data.mongodb.uri}")
   private String MONGO_URI;
-  @Value("${mongodb.database}")
+
+  @Value("${spring.data.mongodb.database}")
   private String MONGO_DB_NAME;
 
   public @Bean MongoClient configure(){
     ConnectionString connString = new ConnectionString(MONGO_URI);
 
     MongoClientSettings settings = MongoClientSettings.builder()
-      .applyConnectionString(connString)
-      .retryWrites(true)
-      .build();
+    .applyConnectionString(connString)
+    .retryWrites(true)
+    .build();
 
     return MongoClients.create(settings);
   }
