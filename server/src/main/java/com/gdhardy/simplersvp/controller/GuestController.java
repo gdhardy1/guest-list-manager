71448aef0ee656c49cb78dc27d3f8d43d3fb1e0b
Gregory D Hardy <gdhardy1@gmail.com>
2020-03-14 12:04:22 -0700

    add swagger documentation for existing routes

--- server/src/main/java/com/gdhardy/simplersvp/controller/GuestController.java
+++ server/src/main/java/com/gdhardy/simplersvp/controller/GuestController.java
@@ -11,21 +11,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 /**
  * GuestController
  */
 @RestController
+@Tag(name="Guest List Management", description="Endpoints for finding, adding, removing, and updating guests")
 public class GuestController { 
 
   @Autowired
   private GuestService guestService;
 
   @GetMapping(value="api/guests", produces="application/json")
+  @Operation(description="Find all guests.", summary="Find all guests.")
   public List<Guest> findGuests(){
     return guestService.findAllGuests();
   }
 
   @PostMapping(value="api/guests", consumes="application/json", produces = "application/json")
+  @Operation(summary="Add a guest.", description="Add a guest.")
   public Guest add(@RequestBody Guest guest) {
     return guestService.add(guest);
   }
