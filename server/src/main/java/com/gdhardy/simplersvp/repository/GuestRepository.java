dee35337d446a738a90594be1659fd23f7b5f7f5
Gregory D Hardy <gdhardy1@gmail.com>
2020-03-27 11:02:05 -0700

    add excluded import

--- server/src/main/java/com/gdhardy/simplersvp/repository/GuestRepository.java
+++ server/src/main/java/com/gdhardy/simplersvp/repository/GuestRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.gdhardy.simplersvp.model.Guest;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
