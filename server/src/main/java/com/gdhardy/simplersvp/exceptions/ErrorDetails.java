c2a9e7c81c14220e8f15dc990613e00eb5e172ec
Gregory D Hardy <gdhardy1@gmail.com>
2020-03-27 08:15:32 -0700

    hide null fields, generalize ErrorDetails

--- server/src/main/java/com/gdhardy/simplersvp/exceptions/ErrorDetails.java
+++ server/src/main/java/com/gdhardy/simplersvp/exceptions/ErrorDetails.java
@@ -1,26 +1,34 @@
 package com.gdhardy.simplersvp.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 /**
  * ErrorDetails
  */
+@JsonInclude(Include.NON_NULL)
 public class ErrorDetails{
-  private String fieldName;
+  private String details;
   private String message;
 
   public ErrorDetails() {
   }
 
-  public ErrorDetails(String fieldName, String message) {
-    this.fieldName = fieldName;
+  public ErrorDetails(String details, String message) {
+    this.details = details;
     this.message = message;
   }
 
-  public String getFieldName() {
-    return this.fieldName;
+  public ErrorDetails(String message){
+    this.message = message;
   }
 
-  public void setFieldName(String fieldName) {
-    this.fieldName = fieldName;
+  public String getDetails() {
+    return this.details;
+  }
+
+  public void setDetails(String details) {
+    this.details = details;
   }
 
   public String getMessage() {
