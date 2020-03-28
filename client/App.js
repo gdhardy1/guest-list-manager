ab01b8c9fad79d359e485123f93237f7a6a6e9a7
Gregory D Hardy <gdhardy1@gmail.com>
2020-03-28 03:49:41 -0700

    implement guest list and form

--- client/App.js
+++ client/App.js
@@ -1,9 +1,37 @@
 import "babel-polyfill";
-import React from "react";
-const ReactDOM = require("react-dom");
+import React, { useState, useEffect, useCallback } from "react";
+import ReactDOM from "react-dom";
+
+import GuestList from "./components/GuestList";
+import GuestForm from "./components/Guestform";
+import axios from "axios";
+import ErrorService from "./service/ErrorService";
 
 function App() {
-  return <div>Hello from React+Spring Boot</div>;
+  const [guests, setGuests] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    const getGuests = async () =>
+      await axios
+        .get("/api/guests")
+        .then(res => {
+          setGuests(res.data);
+        })
+        .catch(ErrorService.handleError);
+
+    getGuests();
+  }, []);
+
+  return (
+    <div>
+      <GuestList guests={guests} />
+      <button className="addGuest" onClick={() => setShowForm(prev => !prev)}>
+        {showForm ? "Hide Form" : "New Guest"}
+      </button>
+      {showForm && <GuestForm />}
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("react"));
